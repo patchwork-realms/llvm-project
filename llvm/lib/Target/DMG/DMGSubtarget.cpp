cc983bcff9adcde571306e0222052a39a02d111c
Akram Ahmad <bulgeypip786@gmail.com>
2023-12-13 00:59:39 +0000

    [DMG]: Fix definition bug

--- llvm/lib/Target/DMG/DMGSubtarget.cpp
+++ llvm/lib/Target/DMG/DMGSubtarget.cpp
@@ -1,10 +1,13 @@
+
 #include "DMGSubtarget.h"
+
 #include "DMG.h"
+#include "DMGTargetMachine.h"
 #include "llvm/MC/TargetRegistry.h"
 
 using namespace llvm;
 
-#define DEBUG_TYPE = "dmg-subtarget"
+#define DEBUG_TYPE "dmg-subtarget"
 
 #define GET_SUBTARGETINFO_TARGET_DESC
 #define GET_SUBTARGETINFO_CTOR
@@ -15,4 +18,13 @@ void DMGSubtarget::anchor() {}
 DMGSubtarget::DMGSubtarget(const Triple &TT, const std::string &CPU,
                            const std::string &FS, const TargetMachine &TM)
         : DMGGenSubtargetInfo(TT, CPU, /*TuneCPU=*/ CPU, FS), InstrInfo(*this),
-          FrameLowering(*this), TLInfo(TM, *this) {}
+          FrameLowering(*this), TLInfo(TM, initializeSubtargetDependencies(CPU, FS, TM)) {
+  ParseSubtargetFeatures(CPU, CPU, FS);
+}
+
+DMGSubtarget &
+DMGSubtarget::initializeSubtargetDependencies(StringRef CPU, StringRef FS,
+                                              const TargetMachine &TM) {
+  ParseSubtargetFeatures(CPU, /* TuneCPU */ CPU, FS);
+  return *this;
+}
